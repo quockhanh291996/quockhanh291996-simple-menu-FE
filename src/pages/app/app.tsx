29657894cf4b53b46c89f9c406608971123df6d7
Khanh Nguyen <khanhqnguyen0209@gmail.com>
2020-03-31 00:09:53 +0700

    feat: Initialize the routes for authentication flow

--- src/pages/app/app.tsx
+++ src/pages/app/app.tsx
@@ -2,17 +2,47 @@ import { CssBaseline } from '@material-ui/core';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 
+import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { muiTheme } from '~common/mui-theme';
+import { APP_PATH } from '~constants/path';
+import { Home } from '~pages/home/home';
+import { StartUpPage } from '~pages/startup/startup';
 
 /**
  * App start from here
  * Basically it does all base configuration before rendering actual app
  */
 export const App: React.FunctionComponent = (): JSX.Element => {
+  const token = '';
+
   return (
     <MuiThemeProvider theme={muiTheme}>
       <CssBaseline />
-      <div></div>
+      <HashRouter>
+          <Switch>
+            <Route exact path={APP_PATH.LOGIN}>
+              {token.length === 0 ? (
+                <StartUpPage />
+              ) : (
+                <Redirect to={APP_PATH.DEFAULT}></Redirect>
+              )}
+            </Route>
+            <Route exact path={APP_PATH.REGISTRATION}>
+              {token.length === 0 ? (
+                <StartUpPage />
+              ) : (
+                <Redirect to={APP_PATH.DEFAULT}></Redirect>
+              )}
+            </Route>
+            <Route>
+              {token.length === 0 ? (
+                <Redirect to={APP_PATH.LOGIN}></Redirect>
+                ) : (
+                <Home />
+              )}
+            </Route>
+          </Switch>
+        </HashRouter>
     </MuiThemeProvider>
   );
 };
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+/**
+ * Startup page here -- It contains the login and registration page.
+ */
+export const Home: React.FC = (): JSX.Element => <div></div>;
