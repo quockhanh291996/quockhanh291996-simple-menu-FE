27ac32f1ba5c42398eee377ff23481aaa5e6c25d
Khanh Nguyen <khanhqnguyen0209@gmail.com>
2020-03-31 01:34:29 +0700

    feat: Create services to interact with login/signup API

---
+++ src/services/features/user.service.ts
@@ -0,0 +1,23 @@
+import { APIHandler } from '~services/http-request/api-response.service';
+
+// List endpoint to get data related to user
+const USER_ENDPOINT = {
+  REGISTER: '/users/',
+  LOGIN: '/token/',
+};
+
+// Export the service to interact with data
+export const UserService = (() => {
+  const pRegister = async (params?: any) => {
+    await APIHandler.post(USER_ENDPOINT.REGISTER, params);
+  };
+
+  const pLogin = async (params?: any) => {
+    await APIHandler.post(USER_ENDPOINT.LOGIN, params);
+  };
+
+  return {
+    register: pRegister,
+    login: pLogin,
+  };
+})();
